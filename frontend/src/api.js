934b0ae45a8518fd05833994cca9db55db5066fe
Yunis-dev <18yabukar@sjbc.wandsworth.sch.uk>
2024-11-07 14:41:50 +0000

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/backend-1385060843:8000";
+window.configs = {
+  apiUrl: '/choreo-apis/backend-1385060843:8000',        
+};
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
